92f7e569cd58ea362ced4cabae2ecf53c3f166b0
RoundPegInASquareHole <alex_pariente@hotmail.es>
2021-03-14 11:10:55 +0100

    creando app...

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var cool = require("cool-ascii-faces");
-console.log(cool());
+var express = require("express");
+var app = express();
+var port = 10000;
+
+app.get('*', (request, response) => {
+    response.redirect('http:3000//' + request.headers.host + request.url);
+});
+
+app.get("/cool", (request,response) => {
+    response.send(cool());
+    console.log("New request to '/cool' has arrived");
+});
+
+app.listen(port, () => {
+    console.log("Server currently listening on port " + port + "...")
+});
