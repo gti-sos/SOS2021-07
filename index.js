48c361ce7048a8b8e87fcab4c5ad914d336f57f0
RoundPegInASquareHole <alex_pariente@hotmail.es>
2021-03-22 00:07:45 +0100

    app server lista

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ var express = require("express");
 var app = express();
 var port = 10000;
 
+var path = require("path");
+
+app.use("/", express.static(path.join(__dirname,"public")));
+
 app.get("/cool", (request,response) => {
     response.send(cool());
     console.log("New request to '/cool' has arrived");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
+});
+
+app.post("/hello", (req, res) => {
+    res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
 });
 
 app.listen(port, () => {
