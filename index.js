12f40c705e4addde855c5715aceef322833dc5a8
nuresctor <escamillatorresnuria@gmail.com>
2021-04-05 22:18:00 +0000

    ultimo cambio

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ app.get(BASE_API_PATH + "/buy_sell", (req,res) => {
 
 var buy_sell_initial = [
     {
-        "autonomous_community":"andalucía",
+        "autonomous_community":"andalucia",
         "province":"sevilla",
         "year":2018,
         "surface":1594.97,
@@ -348,7 +348,7 @@ var buy_sell_initial = [
         "eviction":2.003
     },
     {
-        "autonomous_community":"comunidad de madrid",
+        "autonomous_community":"comunidad_de_madrid",
         "province":"madrid",
         "year":2020,
         "surface":2357.05,
@@ -356,7 +356,7 @@ var buy_sell_initial = [
         "eviction":2.872
     },
     {
-        "autonomous_community":"cataluña",
+        "autonomous_community":"cataluna",
         "province":"barcelona",
         "year":2018,
         "surface":3470.8,
@@ -472,7 +472,7 @@ app.put(BASE_API_PATH + "/buy_sell/:autonomous_community/:year", (req,res)=>{
 	buy_sell = buy_sell.map(i => JSON.stringify(i));
 	buy_sell = new Set(buy_sell);
 	buy_sell = [...buy_sell]
-	buy_sell = buy_sell.map(i => JSON.parse(i))
+	buy_sell = buy_sell.map(i => JSON.parse(i));
 	return res.status(200);
 });
 
@@ -535,15 +535,15 @@ app.get("/info/rentals", (req, res) => {
 app.get("/info/buy_sell", (req, res) => {
     res.send("<html><body><table class='tftable' border='1'>"+
     "<tr><th>autonomous_community</th><th>province</th><th>year</th><th>surface</th><th>annual_variation_percentage</th><th>eviction</th></tr>"+
-    "<tr><td>andalucía</td><td>sevilla</td><td>2018</td><td>1594.97</td><td>9.22</td><td>2.003</td></tr>"+
-    "<tr><td>comunidad de madrid</td><td>madrid</td><td>2020</td><td>2357.05</td><td>6.25</td><td>2.872</td></tr>"+
-    "<tr><td>cataluña</td><td>barcelona</td><td>2018</td><td>3470.8</td><td>9.35</td><td>2.381</td></tr>"+
+    "<tr><td>andalucia</td><td>sevilla</td><td>2018</td><td>1594.97</td><td>9.22</td><td>2.003</td></tr>"+
+    "<tr><td>comunidad_de_madrid</td><td>madrid</td><td>2020</td><td>2357.05</td><td>6.25</td><td>2.872</td></tr>"+
+    "<tr><td>cataluna</td><td>barcelona</td><td>2018</td><td>3470.8</td><td>9.35</td><td>2.381</td></tr>"+
     "<tr><td>navarra</td><td>navarra</td><td>2020</td><td>1400.22</td><td>10.22</td><td>1.057</td></tr>"+
-    "<tr><td>región de murcia</td><td>murcia</td><td>2020</td><td>876.64</td><td>4.32</td><td>1.504</td></tr>"+
-    "<tr><td>andalucía</td><td>málaga</td><td>2018</td><td>1496.23</td><td>14.02</td><td>2.636</td></tr>"+
+    "<tr><td>region_de_murcia</td><td>murcia</td><td>2020</td><td>876.64</td><td>4.32</td><td>1.504</td></tr>"+
+    "<tr><td>andalucia</td><td>malaga</td><td>2018</td><td>1496.23</td><td>14.02</td><td>2.636</td></tr>"+
     "<tr><td>cantabria</td><td>cantabria</td><td>2017</td><td>1202.38</td><td>6.9</td><td>705</td></tr>"+
-    "<tr><td>comunidad valenciana</td><td>valencia</td><td>2017</td><td>1.169.92</td><td>6.23</td><td>2.358</td></tr>"+
-    "<tr><td>andalucía</td><td>granada</td><td>2017</td><td>1250.99</td><td>1.54</td><td>1.013</td></tr>"+
+    "<tr><td>comunidad_valenciana</td><td>valencia</td><td>2017</td><td>1.169.92</td><td>6.23</td><td>2.358</td></tr>"+
+    "<tr><td>andalucia</td><td>granada</td><td>2017</td><td>1250.99</td><td>1.54</td><td>1.013</td></tr>"+
     "</table><//body></html>");
 	
 });
