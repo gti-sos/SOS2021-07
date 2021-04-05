a3ccac34c794a0f6145c44b24e803e035bc26c47
frankys99 <frankys1999@gmail.com>
2021-04-06 00:20:01 +0200

    intento backlog

--- index.js
+++ index.js
@@ -269,20 +269,18 @@ app.put(BASE_API_PATH + "/rentals/:autonomous_community/:year", (req,res)=>{
     
 	
     for(var i in rentals){
-		if(rentals[i].autonomous_community == String(req.params.autonomous_community) && rentals[i].year == String(req.params.year)){
-			var newData = req.body;
-			rentals[i] = newData;
-            break;
-        }
-    }
-		
-	
+		if(rentals[i].autonomous_community == autonomous_community_url && rentals[i].year == year_url){
+			var newRentals = req.body;
+			rentals[i] = newRentals;
+         
+			break;
+		}
+	}
 	rentals = rentals.map(i => JSON.stringify(i));
 	rentals = new Set(rentals);
 	rentals = [...rentals]
 	rentals = rentals.map(i => JSON.parse(i))
-	return res.status(200);
-
+	res.status(200).send("CHANGE MADE");
 });
    
 
