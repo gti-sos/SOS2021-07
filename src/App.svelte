07bde36b942f7ec7fc83d7d7e05d7e915f075bcc
frankys99 <frankys1999@gmail.com>
2021-04-25 22:18:22 +0200

    intento backlog

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+
+import Error404 from "./Error404.svelte";
+import Init from "./Init.svelte";
+import Router from 'svelte-spa-router';
+import RentalsAPI from "./RentalsAPI.svelte";
+
+
+	const componentes = {
+		"/":Init,
+		"/rentals": RentalsAPI,
+		'*': Error404};
+</script>
+
+<main>
+	<Router {componentes}> </Router>
+</main>
+
+<style>
+	
+</style>
---
+++ src/Error404.svelte
@@ -0,0 +1,34 @@
+<script>
+	import {
+		Button
+	  } from "sveltestrap";
+	</script>
+	
+	<main>
+	  <h2>404</h2>
+	<h4>Vaya! No hemos encontrado lo que nos pides</h4>
+	<p>La página que buscabas no existe. Comprueba que hayas escrito bien la dirrección.</p>
+	<p>Disculpe las molestias</p>
+	  <a href="/">
+		<Button color="primary">Volver a la página Inicial</Button></a>
+	
+	</main>
+	
+	<style>
+		main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		max-width: 380px;
+	  }
+	  h2{
+		font-size: 100px;
+	  }
+	  h4{
+		background-color: grey;
+		color: white;
+		padding: 10px;
+		margin: 5px;
+		
+	  }
+	</style>
