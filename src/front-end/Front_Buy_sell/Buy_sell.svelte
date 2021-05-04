5c36b270c5f5135b272afcb0e180f11c19f6b48d
nuresctor <escamillatorresnuria@gmail.com>
2021-05-04 18:38:10 +0000

    arreglo 1

--- src/front-end/Front_Buy_sell/Buy_sell.svelte
+++ src/front-end/Front_Buy_sell/Buy_sell.svelte
@@ -12,45 +12,15 @@
     Pagination,
     PaginationItem,
     PaginationLink,
+    UncontrolledAlert ,
   } from "sveltestrap";
   import { onMount } from "svelte";
-
-  //------------------Nav------------------------------------------------
-
-  //Load stats
-  let open1 = false;
-  const toggle1 = () => (open1 = !open1);
-  const toggle1P = () => {
-    open1 = !open1;
-    loadStats();
-  };
-  //Delete all stats
-  let open2 = false;
-  const toggle2 = () => (open2 = !open2);
-  const toggle2P = () => {
-    open2 = !open2;
-    deleteAllStats();
-  };
-edit
-  //------------------API-------------------
-  const BASE_CONTACT_API_PATH = "/api/v1";
-
-  let buy_sell_Data = [];
-
-  let newData = {
-    autonomous_community: "",
-    province: "",
-    year: "",
-    surface: "",
-   	annual_variation_percentage: "",
-    eviction: "",
-  };
 
   //Alertas
   let errorMsg = "";
   let okMsg = "";
   let errorStatus = 0;
-
+  
   let fullQuery = "";
 
   //Pagination
@@ -61,30 +31,58 @@ edit
   let last_page = 1;
   let total = 0;
 
+  let newData = {
+    autonomous_community: "",
+    province: "",
+    year: "",
+   	"surface": "",
+ 	"annual_variation_percentage": "",
+    "eviction": ""
+  };
+
+  // Nav
+
+  //Load
+  let open1 = false;
+  const toggle1 = () => (open1 = !open1);
+  const toggle1P = () => {
+    open1 = !open1;
+    loadStats();
+  };
+  //Delete
+  let open2 = false;
+  const toggle2 = () => (open2 = !open2);
+  const toggle2P = () => {
+    open2 = !open2;
+    deleteStats();
+  };
+
+  //Alerts
+  let visible = true;
+
+  //API
+  let buy_sell_Data = [];
+  let error = null;
+
   //Functions
 
   async function loadStats() {
     console.log("Loading data...");
-    const res = await fetch(
-      BASE_CONTACT_API_PATH + "/buy_sell/loadInitialData"
-    ).then(function (res) {
-      if (res.ok) {
-        console.log("OK");
-        getStats();
-        //Alertas
-        errorMsg = "";
-        errorStatus = 0;
-        okMsg = "Datos cargados correctamente";
-      } else {
-        if (res.status === 409) {
-          errorMsg = "Alguno de los datos ya se encuentra cargados";
-        } else if (res.status === 500) {
-          errorMsg = "No se ha podido acceder a la base de datos";
+    const res = await fetch("/api/v1/buy_sell/loadInitialData").then(
+      function (res) {
+        if (res.ok) {
+          console.log("OK");
+          getStats();
+          error = 0;
+        } else if (res.status == 409) {
+          error = 409;
+          console.log("Conflict");
+        } else {
+          error = 404;
+          console.log("Error");
         }
-        okMsg = "";
-        console.log("ERROR!" + errorMsg);
       }
-    });
+    );
   }
 
   async function searchStat() {
@@ -127,10 +125,10 @@ edit
     }
   }
 
-  //Total de datos en la BD
-  
+    //Total de datos en la BD
   async function getNumTotal() {
-    const res = await fetch(BASE_CONTACT_API_PATH + "/buy_sell");
+    const res = await fetch(
+      "/api/v1/buy_sell");
     if (res.ok) {
       const json = await res.json();
       total = json.length;
@@ -141,13 +139,11 @@ edit
     }
   }
   //Calcula el rango entre ods valores
-  
   function range(size, startAt = 0) {
     return [...Array(size).keys()].map((i) => i + startAt);
   }
 
-  //Cambio de pagina
-  
+    //Cambio de pagina
   function changePage(page, offset) {
     console.log("------Change page------");
     console.log("Params page: " + page + " offset: " + offset);
@@ -168,12 +164,10 @@ edit
   async function getStats() {
     console.log("Fetching data...");
 
-    const res = await fetch(
-      BASE_CONTACT_API_PATH +
-        "/buy_sell?limit=" +
-        limit +
-        "&offset=" +
-        current_offset
+    const res = await fetch("/api/v1/buy_sell?limit=" +
+      limit + 
+      "&offset=" +
+      current_offset
     );
 
     if (res.ok) {
@@ -181,56 +175,45 @@ edit
       const json = await res.json();
       buy_sell_Data = json;
       console.log(`We have received ${buy_sell_Data.length} stats.`);
-      errorMsg = "";
       getNumTotal();
     } else {
-      if (buy_sell_Data.length != 0) {
-        errorMsg = "No hay datos disponibles";
-        console.log("ERROR!");
-      }
-      if (res.status === 500) {
-        errorMsg = "No se han podido acceder a la base de datos";
-      }
-      okMsg = "";
-      console.log("ERROR!" + errorMsg);
+      console.log("Error");
     }
+    init = false;
   }
 
-  async function deleteAllStats() {
+  async function deleteStats() {
     console.log("Deleting data...");
 
-    const res = await fetch(BASE_CONTACT_API_PATH + "/buy_sell/", {
+    const res = await fetch("/api/v1/buy_sell/", {
       method: "DELETE",
     }).then(function (res) {
       if (res.ok) {
         console.log("OK");
         buy_sell_Data = [];
-        errorMsg = "";
-        okMsg = "Operación realizada correctamente";
+        error = 0;
+      } else if (res.status = 404) {
+        error = 404;
+        console.log("ERROR Data not found in database");
       } else {
-        if(res.status===404){
-          errorMsg = "No existen datos que borrar";
-        }else if(res.status ===500){
-          errorMsg = "No se han podido acceder a la base de datos";
-        }        
-        okMsg = "";
-        console.log("ERROR!" + errorMsg);
+        error = 1000;
+        console.log("ERROR");
       }
     });
   }
 
- async function deleteStat(province, year) {
-    console.log(`Deleting data with name ${province} and date ${year}`);
+  async function deleteStat(autonomous_community, province, year) {
+    console.log(`Deleting data with name ${autonomous_community}, ${province} and date ${year}`);
 
     const res = await fetch(
-      BASE_CONTACT_API_PATH + "/buy_sell/" + province + "/" + year,
+      "/api/v1/buy_sell/" + autonomous_community + "/" + province + "/" + year,
       {
         method: "DELETE",
       }
     ).then(function (res) {
       if (res.ok) {
         console.log("OK");
-        if (buy_sell_Data.length === 1) {
+        if (buy_sell_Data.length == 1) {
           buy_sell_Data = [];
           currentPage = 1;
         }
@@ -238,9 +221,9 @@ edit
         okMsg = "Operación realizada correctamente";
         getStats();
       } else {
-        if(res.status===404){
+        if(res.status==404){
           errorMsg = "No existe el dato a borrar";
-        }else if(res.status ===500){
+        }else if(res.status ==500){
           errorMsg = "No se han podido acceder a la base de datos";
         }        
         okMsg = "";
@@ -252,14 +235,15 @@ edit
   async function insertStat() {
     console.log("Inserting stat: " + JSON.stringify(newData));
 
-	newData.autonomous_community = newData.autonomous_community;
+    newData.autonomous_community = newData.autonomous_community;
     newData.province = newData.province;
     newData.year = parseInt(newData.year);
-    newData.surface = parseFloat(newData.surface);
-	 newData.annual_variation_percentage = parseFloat(newData.annual_variation_percentage);
-     newData.eviction = parseFloat(newData.eviction);
+    newData["surface"] = parseFloat(newData["surface"]);
+    newData["annual_variation_percentage"] = parseFloat(newData["annual_variation_percentage"]);
+	newData["eviction"] = parseFloat(newData["eviction"]);
 
-    const res = await fetch(BASE_CONTACT_API_PATH + "/buy_sell/", {
+    const res = await fetch(
+      "/api/v1/buy_sell/", {
       method: "POST",
       body: JSON.stringify(newData),
       headers: {
@@ -321,32 +305,49 @@ edit
       {/if}
     </NavItem>
   </Nav>
-  <h2>Alquileres</h2>
+  <h2>Datos Compra-Venta</h2>
 
-  <p />
+ 
+  <!-- Alerts -->
+    {#if error === 0}
+      <UncontrolledAlert  color="success" >
+          Operación realizada correctamente.
+        
+      </UncontrolledAlert>
+    {/if}
 
-  <p />
-  {#if errorMsg}
-    <p style="color: red">ERROR: {errorMsg}</p>
-  {/if}
-  {#if okMsg}
-    <p style="color: green">{okMsg}</p>
-  {/if}
-
-  <!-- Table -->
-  <Table borderer>
-    <thead>
-      <tr>
+    {#if error === 409}
+      <UncontrolledAlert  color="warning" >
+          Los datos ya se encuentran cargados.
+        
+      </UncontrolledAlert>
+    {:else if error === 404}
+      <UncontrolledAlert  color="danger">
+          No se encuentra en la base de datos.
+        
+      </UncontrolledAlert>
+    {:else if error ===1000}
+      <UncontrolledAlert  color="danger" >
+       Error desconocido.
+      </UncontrolledAlert>
+    {/if}
+  
+<!-- Table -->
+  {#if buy_sell_Data.length === 0}
+    <p>No se han encontrado datos, por favor carga los datos iniciales o inserte uno usted mismo/a.</p>
+    <Table borderer>
+      <thead>
+        <tr>
         <th> Comunidad Autónoma </th>
         <th>Provincia </th>
         <th>Año </th>
         <th>Superficie </th>
         <th>Porcentaje de variación anual </th>
         <th>Desalojo </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
         <td
           ><input
             type="text"
@@ -365,7 +366,6 @@ edit
           ><input
             type="number"
             placeholder=""
-            min="1900"
             bind:value={newData.year}
           /></td
         >
@@ -373,58 +373,115 @@ edit
           ><input
             type="number"
             placeholder=""
-            min="1.0"
-            bind:value={newData.surface}
+            bind:value={newData["surface"]}
           /></td
         >
         <td
           ><input
             type="number"
             placeholder=""
-            min="1.0"
-            bind:value={newData.annual_variation_percentage}
+            bind:value={newData["annual_variation_percentage"]}
           /></td
         >
         <td
           ><input
             type="number"
             placeholder=""
-            min="1.0"
-            bind:value={newData.eviction}
+            bind:value={newData["eviction"]}
+          /></td
+        >   
+        <td><Button color="primary" on:click={insertStat}>Insertar</Button></td>
+        </tr>
+        </tbody>
+        </Table>
+
+  {:else}
+    <Table borderer>
+      <thead>
+        <tr>
+          <th> Comunidad Autónoma </th>
+          <th>Provincia </th>
+          <th>Año </th>
+          <th>Superficie </th>
+          <th>Porcentaje de variación anual </th>
+          <th>Desalojo </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        <td
+          ><input
+            type="text"
+            placeholder=""
+            bind:value={newData.autonomous_community}
           /></td
         >
-        
+        <td
+          ><input
+            type="text"
+            placeholder=""
+            bind:value={newData.province}
+          /></td
+        >
+        <td
+          ><input
+            type="number"
+            placeholder=""
+            bind:value={newData.year}
+          /></td
+        >
+        <td
+          ><input
+            type="number"
+            placeholder=""
+            bind:value={newData["surface"]}
+          /></td
+        >
+        <td
+          ><input
+            type="number"
+            placeholder=""
+            bind:value={newData["annual_variation_percentage"]}
+          /></td
+        >
+        <td
+          ><input
+            type="number"
+            placeholder=""
+            bind:value={newData["eviction"]}
+          /></td
+        >   
         <td><Button color="primary" on:click={insertStat}>Insertar</Button></td>
         <td>
           <Button color="secondary" on:click={searchStat}>Buscar</Button>
         </td>
       </tr>
-
-      {#each buy_sell_Data as data}
-        <tr>
-          <td>{data.autonomous_community}</td>
-          <td>{data.province}</td>
-          <td>{data.year}</td>
-          <td>{data.surface}</td>
-          <td>{data.annual_variation_percentage}%</td>
-          <td>{data.eviction}</td>
-          <td>
-            <a href="#/buy_sell/{data.province}/{data.year}">
+      
+        {#each buy_sell_Data as stat}
+          <tr>
+            <td>{stat.autonomous_community}</td>
+            <td>{stat.province }</td>
+            <td>{stat.year}</td>
+            <td>{stat['surface']}</td>
+            <td>{stat['annual_variation_percentage']}</td>
+            <td>{stat['eviction']}</td>
+            <td>
+            <a href="#/buy_sell/{stat.autonomous_community}/{stat.province}/{stat.year}">
               <Button color="primary">Editar</Button>
             </a></td
           >
           <td
             ><Button
               color="danger"
-              on:click={deleteStat(data.province, data.year)}>Borrar</Button
+              on:click={deleteStat(stat.autonomous_community, stat.province, stat.year)}>Borrar</Button
             ></td
           >
-        </tr>
-      {/each}
-    </tbody>
-  </Table>
+          </tr>
+        {/each}
+      </tbody>
+    </Table>
 
-  <!-- Pagination -->
+    <!-- Pagination -->
   <Pagination ariaLabel="Web pagination">
     <PaginationItem class={current_page === 1 ? "disabled" : ""}>
       <PaginationLink
@@ -451,6 +508,7 @@ edit
       />
     </PaginationItem>
   </Pagination>
+  {/if}
 </main>
 
 <style>
