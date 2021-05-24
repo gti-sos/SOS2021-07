ca339ba59ef711c2994be533fb471b908d20dfd8
nuresctor <escamillatorresnuria@gmail.com>
2021-05-24 03:08:04 +0000

    nuevo fichero3

--- src/front-end/Front_Buy_sell/integraciones.svelte
+++ src/front-end/Front_Buy_sell/integraciones.svelte
@@ -1,22 +1,7 @@
-<svelte:head>
-  <script src="https://cdn.plot.ly/plotly-latest.min.js" on:load="{loadGraph}"></script>
-</svelte:head>
-
-<main>
-  <h2 style="text-align: center;"> <i class="fas fa-car"></i> Alquileres en España</h2>
-
-  <Nav>
-    <NavItem>
-      <NavLink href="/"><Button color="primary">Página Inicial</Button></NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink href="/#/rentals"><Button color="primary">Datos</Button></NavLink>
-    </NavItem>
-</Nav>
-
-  <div id="graph"></div>
-  <p>Muestra datos sobre el alquiler en provincias de España</p>
-</main>
+<script>
+	// logic goes here
+</script>
 
 <style>
+	/* styles go here */
 </style>
