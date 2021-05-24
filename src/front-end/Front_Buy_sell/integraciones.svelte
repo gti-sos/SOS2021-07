0463aab36116d461da4421add6b467e0ed16617c
nuresctor <escamillatorresnuria@gmail.com>
2021-05-24 11:02:11 +0000

    fichero integracion

--- src/front-end/Front_Buy_sell/integraciones.svelte
+++ src/front-end/Front_Buy_sell/integraciones.svelte
@@ -1,7 +1,81 @@
 <script>
-	// logic goes here
+  import {
+    Button,
+    Icon,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Col,
+    Container,
+    Nav,
+    NavItem,
+    NavLink,
+    Row,
+  } from "sveltestrap";
 </script>
 
+<main>
+  <Nav>
+    <NavItem>
+      <NavLink href="/">Volver</NavLink>
+    </NavItem>
+  </Nav>
+
+  <div>
+  <h1>Integrations</h1>
+</div>
+<div>
+  <Card class="mb-3">
+    <CardHeader>
+      <CardTitle><h1>INTEGRACIONES</h1></CardTitle>
+    </CardHeader>
+    <CardBody>
+      <CardSubtitle />
+      <CardText />
+      <a href="/#/integrations/restcountries"
+        ><Button color="primary">API1</Button></a
+      >
+      <a href="/#/integrations/coinCap"
+        ><Button color="primary">API2</Button></a
+      >
+      <a href="/#/integrations/sanityStats">
+        <Button color="primary">API3</Button></a
+      >
+      <a href="/#/integrations/platformsStats">
+        <Button color="primary">API4</Button></a
+      >
+    </CardBody>
+  </Card>
+</div>
+
+</main>
+
 <style>
-	/* styles go here */
+   main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  div{
+    margin-bottom: 15px;
+  }
+  b {
+    color: #00c1ff;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
