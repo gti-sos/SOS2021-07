b157a7133cb006843d97a3597610d40968b00bc1
nuresctor <escamillatorresnuria@gmail.com>
2021-05-24 11:44:25 +0000

    fichero integracion8

--- src/front-end/Front_Buy_sell/Buy_sell_Integraciones.svelte
+++ src/front-end/Front_Buy_sell/Buy_sell_Integraciones.svelte
@@ -19,44 +19,7 @@
    import { TabContent, TabPane } from 'sveltestrap';
 </script>
 
-<main>
-  <Nav>
-    <NavItem>
-      <NavLink href="/">Volver</NavLink>
-    </NavItem>
-  </Nav>
-
-  
-<div>
-  <Card class="mb-3">
-    <CardHeader>
-      <CardTitle><h1>INTEGRACIONES</h1></CardTitle>
-    </CardHeader>
-    <CardBody>
-      <CardSubtitle />
-      <CardText />
-      <a href=""
-        ><Button color="primary">API1</Button></a
-      >
-      <a href=""
-        ><Button color="primary">API2</Button></a
-      >
-      <a href="">
-        <Button color="primary">API3</Button></a
-      >
-      <a href="">
-        <Button color="primary">API4</Button></a
-      >
-    </CardBody>
-  </Card>
-</div>
-
-<br>
-<br>
-<br>
-
-<div>
-	<TabContent>
+<TabContent>
   <TabPane tabId="alpha" tab="Alpha" active>
     <h2>Alpha</h2>
     <img
@@ -76,32 +39,3 @@
       src="https://upload.wikimedia.org/wikipedia/en/2/22/Charlie_Brown.png">
   </TabPane>
 </TabContent>
-</div>
-
-</main>
-
-<style>
-   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  div{
-    margin-bottom: 15px;
-  }
-  b {
-    color: #00c1ff;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
