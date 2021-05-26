b0cfff50639504cb072050ee94b9a3a969eda74c
frankys99 <frankys1999@gmail.com>
2021-05-26 17:00:16 +0200

    prueba

--- src/front-end/integraciones/rentals/Grupo27.svelte
+++ src/front-end/integraciones/rentals/Grupo27.svelte
@@ -1,96 +1,133 @@
 <script>
-    import {pop} from "svelte-spa-router";
-  import Button from "sveltestrap/src/Button.svelte";
-  async function loadGraph() {
-      const resDatasugarconsume = await fetch("https://sos2021-27.herokuapp.com/api/v2/province-budget-and-investment-in-social-promotion");
-      const resDataEquality = await fetch("https://sos2021-07.herokuapp.com/api/v1/integration/rentals");
-      let sugarconsume = await resDatasugarconsume.json();
-      let equality = await resDataEquality.json();
-      console.log(equality);
-      let datasugarconsume = sugarconsume.map((d) => {
-          let res = {
-              name: d.province + " - " + d.year,
-              value: parseInt(d["invest_promotion"])
-          };
-          return res;
-      });
-      let dataEquality = equality.map((d) => {
-          let res = {
-              name: d.province + " - " + d.year,
-              value: d["rent"]
-          };
-          return res;
-      });
-      let dataTotal =
-          [
-              {
-                  name: "Consumo de azucar",
-                  data: datasugarconsume
-              },
-              {
-                  name: "Ranking de paz",
-                  data: dataEquality
-              }
-          ];
-      Highcharts.chart('container', {
-          chart: {
-              type: 'packedbubble',
-              height: '60%'
-          },
-          title: {
-              text: 'Relación entre el ranking de paz y consumo de azucar'
-          },
-          tooltip: {
-              useHTML: true,
-              pointFormat: '<b>{point.name}:</b> {point.value}'
-          },
-          plotOptions: {
-              packedbubble: {
-                  minSize: '20%',
-                  maxSize: '100%',
-                  zMin: 0,
-                  zMax: 100,
-                  layoutAlgorithm: {
-                      splitSeries: false,
-                      gravitationalConstant: 0.02
-                  },
-                  dataLabels: {
-                      enabled: true,
-                      format: '{point.name}',
-                      filter: {
-                          property: 'y',
-                          operator: '>',
-                          value: 250
-                      },
-                      style: {
-                          color: 'black',
-                          textOutline: 'none',
-                          fontWeight: 'normal'
-                      }
-                  }
-              }
-          },
-          series: dataTotal
-      });
-  }
-  loadGraph();
+    import Button from "sveltestrap/src/Button.svelte";
+    import { pop } from "svelte-spa-router";
+    
+    async function loadChart(){
+        var myDataTrans={
+            name: 'Variacion de la Renta',
+            data: []
+        };
+        var extDataTrans={
+            name: 'Inversion Social',
+            data: []
+        };
+        var allData =[];
+        const resData = await fetch("/api/v1/rentals");
+        const MyData = await resData.json();  
+
+        const res2Data = await fetch("https://education-expenditures.herokuapp.com/api/v1");
+        const extData = await res2Data.json();  
+        
+        MyData.forEach((v) => {
+        myDataTrans['data'].push({
+            name:v.province  + " " +v.year,
+            value: v["rent"]
+            });
+        });
+
+        extData.forEach((v) => {
+        extDataTrans['data'].push({
+            name: v.country + " " + v.year,
+            value: v.education_expenditure_per_millions
+            });
+        });
+        
+        allData.push(myDataTrans);
+        allData.push(extDataTrans);
+        
+        Highcharts.chart('container', {
+        chart: {
+            type: 'packedbubble',
+            height: '85%'
+        },
+        title: {
+            text: 'Renta y Gasto en educación por millones de euros.'
+        },
+        tooltip: {
+            useHTML: true
+        },
+        plotOptions: {
+            packedbubble: {
+                minSize: '20%',
+                maxSize: '70%',
+                zMin: 0,
+                zMax: 1000,
+                layoutAlgorithm: {
+                    gravitationalConstant: 0.02,
+                    splitSeries: true,
+                    seriesInteraction: false,
+                    dragBetweenSeries: true,
+                    parentNodeLimit: true
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}',
+                    filter: {
+                        property: 'y',
+                        operator: '>',
+                        value: 200
+                    },
+                    style: {
+                        color: 'black',
+                        textOutline: 'none',
+                        fontWeight: 'normal'
+                    }
+                }
+            }
+        },
+        series: allData
+    });
+}
 </script>
-
 <svelte:head>
-
-
+  <script src="https://code.highcharts.com/highcharts.js" on:load={loadChart}></script>
+  <script src="https://code.highcharts.com/modules/series-label.js"></script>
+  <script src="https://code.highcharts.com/highcharts-more.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  <script
+    src="https://code.highcharts.com/modules/accessibility.js"></script>
 </svelte:head>
 
-<main>
 
-  <figure class="highcharts-figure">
-      <div id="container"></div>
-      <p class="highcharts-description">
-          Gráfica de diferencia entre el consumo de azucar y el ranking de paz.
-      </p>
-  </figure>
-  <div style="text-align:center;padding-bottom: 3%;">
-      <Button outline align = "center" color="secondary" on:click="{pop}">Volver</Button>
-      </div>
-      
+
+<main>
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">
+            Esta grafica muestra el total de niños y niñas que abandona la escuela y el gasto en educación por millones de euros en los últimos años.
+        </p>
+    </figure>
+    <Button outline color="secondary" on:click="{pop}"> Atrás</Button>
+    
+
 </main>
+
+<style>
+    .highcharts-figure, .highcharts-data-table table {
+    min-width: 320px; 
+    max-width: 800px;
+    margin: 1em auto;
+    }
+    .highcharts-data-table table {
+        font-family: Verdana, sans-serif;
+        border-collapse: collapse;
+        border: 1px solid #EBEBEB;
+        margin: 10px auto;
+        text-align: center;
+        width: 100%;
+        max-width: 500px;
+    }
+    .highcharts-data-table caption {
+        padding: 1em 0;
+        font-size: 1.2em;
+        color: #555;
+    }
+    .highcharts-data-table th {
+        font-weight: 600;
+        padding: 0.5em;
+    }
+    .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+        padding: 0.5em;
+    }
+</style>
