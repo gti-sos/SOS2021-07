f76a2eab60185df01f698f2d1ed9888d40b1d76d
nuresctor <escamillatorresnuria@gmail.com>
2021-05-27 15:42:27 +0000

    xfabor funciona6

--- src/front-end/Front_Buy_sell/Buy_sell_Chart_ZING.svelte
+++ src/front-end/Front_Buy_sell/Buy_sell_Chart_ZING.svelte
@@ -34,7 +34,7 @@ let estrin='';
     let chartConfig = {
       type: 'wordcloud',
       options: {
-        text: 'We the people of the United States, in order to form a more perfect union, establish justice, insure domestic tranquility, provide for the common defense, promote the general welfare, and secure the blessings of liberty to ourselves and our posterity, do ordain and establish this Constitution for the United States of America.',
+        text: estrin,
         aspect: 'spiral',
         colorType: 'palette',
         ignore: ['establish', 'this'],
