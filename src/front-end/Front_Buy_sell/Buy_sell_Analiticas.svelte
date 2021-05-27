91bfeeaa52dfd70636fa3ac05597cf080e9898a6
nuresctor <escamillatorresnuria@gmail.com>
2021-05-27 10:22:34 +0000

    holO4

--- src/front-end/Front_Buy_sell/Buy_sell_Analiticas.svelte
+++ src/front-end/Front_Buy_sell/Buy_sell_Analiticas.svelte
@@ -48,14 +48,6 @@ import {
   
   <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Festudianteo.com%2Fmatematicas%2Fgraficas-concepto-y-caracteristicas%2F&psig=AOvVaw2Wr-I4j9s2-vzwFiw7gSGD&ust=1622194820500000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCig-fI6fACFQAAAAAdAAAAABAK" alt="GRF" width="500" height="600">
 
-  <div>
-    {#if errorMsg}
-      <p class="msgRed" style="color: #9d1c24">ERROR: {errorMsg}</p>
-    {/if}
-    {#if okMsg}
-      <p class="msgGreen" style="color: #155724">{okMsg}</p>
-    {/if}
-  </div>
   
 </main>
 
