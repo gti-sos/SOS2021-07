2c1e4217ceac1658a2ee32fdbd4a9063b67bcc09
frankys99 <frankys1999@gmail.com>
2021-05-30 19:39:31 +0200

    Covid

--- src/front-end/integraciones/rentals/Covid.svelte
+++ src/front-end/integraciones/rentals/Covid.svelte
@@ -1,92 +1,143 @@
-
 <script>
+	import {pop} from "svelte-spa-router";
     import Button from "sveltestrap/src/Button.svelte";
-    async function loadGraph() {
-        
-        const resDataEduacionGastos = await fetch("https://covid19.secuoyas.io/api/v1/es/ccaa?ultimodia=true");
+	async function loadGraph(){
+
+
+	    let Externa2 = [];
+    
+
+
+        const DatosExternos = await fetch("https://coronavirus-19-api.herokuapp.com/countries");
+		if (DatosExternos.ok) {
+			console.log("Externa2 cargado");
+			const json = await DatosExternos.json();
+            Externa2 = json;
+			console.log(Externa2)
+		} else {
+			console.log("ERROR!");
+        }
+		let aux = []
+        let valores = []
         
 
-        
-        let Data1 = await resDataEduacionGastos.json();
-        
+		Externa2.forEach((x) => {
+            if((x.country=="Mexico"||x.country=="Canada")){	
+				aux={
+					name: x.country,
+					data: [parseInt(x.deaths),parseInt(x.recovered),0,0]
+				}
+				valores.push(aux)
+			}  	
+		
 
-       
-        let dataGastos_Educacion = Data1.map((x) => {
-            let res = {
-                name: x.nombreLugar + " - " +  "( hoy )",
-                value: x["casosRecuperadosDiario"]
-            };
-            return res;
         });
-        
-        let dataTotal =
-            [
-                
-                {
-                    name: "Gasto en educación por capital",
-                    data: dataGastos_Educacion
-                }
-            ];
-        Highcharts.chart('container', {
-            chart: {
-                type: 'packedbubble',
-                height: '40%'
-            },
+    Highcharts.chart('container', {
+        chart: {
+            type: 'areaspline'
+        },
+        title: {
+            text: 'G01 - Externa2'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF'
+        },
+        xAxis: {
+            categories: [
+                'deaths',
+                'recovered',
+
+            ],
+            plotBands: [{ // visualize the weekend
+                from: 4.5,
+                to: 6.5,
+                color: 'rgba(68, 170, 213, .2)'
+            }]
+        },
+        yAxis: {
             title: {
-                text: 'Mezcla de APIs'
-            },
-            tooltip: {
-                useHTML: true,
-                pointFormat: '<b>{point.name}:</b> {point.value}'
-            },
-            plotOptions: {
-                packedbubble: {
-                    minSize: '30%',
-                    maxSize: '120%',
-                    zMin: 0,
-                    zMax: 500,
-                    layoutAlgorithm: {
-                        splitSeries: false,
-                        gravitationalConstant: 0.02
-                    },
-                    dataLabels: {
-                        enabled: true,
-                        format: '{point.name}',
-                        filter: {
-                            property: 'y',
-                            operator: '>',
-                            value: 250
-                        },
-                        style: {
-                            color: 'black',
-                            textOutline: 'none',
-                            fontWeight: 'normal'
-                        }
-                    }
-                }
-            },
-            series: dataTotal
-        });
-    }
-    loadGraph();
+                text: 'Unidades'
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' units'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series:  valores
+        
+    });
+}
 </script>
 
-<svelte:head>
-
-    <script src="https://code.highcharts.com/highcharts.js" on:load={loadGraph}></script>
-    <script src="https://code.highcharts.com/highcharts-more.js" on:load={loadGraph}></script>
-    <script src="https://code.highcharts.com/modules/exporting.js" on:load={loadGraph}></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadGraph}></script>
-
-</svelte:head>
-
+    <svelte:head>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+        <script src="https://code.highcharts.com/modules/export-data.js"></script>
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+    </svelte:head>
 <main>
-
     <figure class="highcharts-figure">
         <div id="container"></div>
-        <p class="highcharts-description" align ="center">
-            Gráfica muestra los datos de en conjunto del porcentaje de salio por provincias de españa y el gasto en educacion segun capital en paises del mundo
+        <p class="highcharts-description">
+            Muertes y recuperaciones a causa del covid-19
         </p>
+        <Button outline color="secondary" on:click="{pop}"> <i class="fas fa-arrow-circle-left"></i> Atrás </Button>
     </figure>
-
 </main>
+
+<style>
+#container {
+    height: 400px; 
+}
+
+.highcharts-figure, .highcharts-data-table table {
+    min-width: 310px; 
+    max-width: 800px;
+    margin: 1em auto;
+}
+
+.highcharts-data-table table {
+    font-family: Verdana, sans-serif;
+    border-collapse: collapse;
+    border: 1px solid #EBEBEB;
+    margin: 10px auto;
+    text-align: center;
+    width: 100%;
+    max-width: 500px;
+}
+.highcharts-data-table caption {
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+    padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+    padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+    background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+    background: #f1f7ff;
+}
+
+</style>
