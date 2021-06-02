728c4e5042917f5850e4a727e176377666f80e86
frankys99 <frankys1999@gmail.com>
2021-06-02 19:48:30 +0200

    Cartas

--- src/front-end/integraciones/rentals/Ext2_cartas.svelte
+++ src/front-end/integraciones/rentals/Ext2_cartas.svelte
@@ -44,7 +44,7 @@ async function loadChart(){
             label: "Numero de cartas con ese atributo",
             data: numeroCarta,
             backgroundColor: [
-              "rgba(155, 99, 132, 0.2)",
+              "rgba(155, 29, 32, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
               "rgba(75, 192, 192, 0.2)",
@@ -52,7 +52,7 @@ async function loadChart(){
               "rgba(255, 159, 64, 0.2)",
             ],
             borderColor: [
-              "rgba(255, 99, 132, 1)",
+              "rgba(155, 29, 32, 1)",
               "rgba(54, 162, 235, 1)",
               "rgba(255, 206, 86, 1)",
               "rgba(75, 192, 192, 1)",
