516645973d79477e3e19c518dbeda8693e9000e6
nuresctor <escamillatorresnuria@gmail.com>
2021-06-04 14:11:30 +0000

    onload

--- src/front-end/Front_Buy_sell/Buy_sell_IP.svelte
+++ src/front-end/Front_Buy_sell/Buy_sell_IP.svelte
@@ -90,7 +90,7 @@
 <script src="https://code.highcharts.com/modules/series-label.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
 	
 </svelte:head>
 <main>
