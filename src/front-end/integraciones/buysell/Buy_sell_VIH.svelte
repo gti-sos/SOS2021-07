35c1655a5e0cb349751f2f21815480d9633b0c37
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 12:35:05 +0000

    funcionara5

--- src/front-end/integraciones/buysell/Buy_sell_VIH.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_VIH.svelte
@@ -2,11 +2,12 @@
   import { onMount } from "svelte";
   import { Table, Button, Nav, NavItem, NavLink } from "sveltestrap";
   
-let HIVData = [];
-let yearData = [];
-let living_withData=[];
-let newly_infectedData=[];
-let total_infectedData=[];
+let buyData = [];
+let evictionData = [];
+let desaData=[];
+let totalData=[];
+let cont=[];
+let ano=2018;
 
 let buy_sell_Chart_surface_Data = [];
 let buy_sell_Chart_eviction_Data = [];
@@ -23,7 +24,7 @@ let buy_sell_Chart_eviction_Data = [];
    async function loadStats() {
     
     const res = await fetch(
-     "https://sos2021-24.herokuapp.com/api/v2/children-with-hiv/loadInitialData"
+     "https://sos2021-07.herokuapp.com/api/v2/buy_sell/loadInitialData"
     ).then(function (res) {
       if (res.ok) {
         loadChart();
@@ -43,88 +44,114 @@ let buy_sell_Chart_eviction_Data = [];
   
  async function loadChart() {
     console.log("Fetching data...");
-    const res = await fetch("https://sos2021-24.herokuapp.com/api/v2/children-with-hiv?country=Ethiopia");
-    HIVData = await res.json();
+	// cogemos para año 2018
+    const res = await fetch("https://sos2021-07.herokuapp.com/api/v2/buy_sell?year=2018");
+    buyData = await res.json();
     if (res.ok) {
 	
-      HIVData.forEach(stat => {
+      buyData.forEach(stat => {
 	  
-	  yearData.push(parseInt(stat.year));
-	  living_withData.push(parseInt(stat.living_with));
-	  newly_infectedData.push(parseInt(stat.newly_infected));
-	  total_infectedData.push(parseInt(stat.total_infected));
+	  evictionData.push(parseInt(stat.eviction));
       
       });
     }
-    
-    console.log(yearData);
-	console.log(living_withData);
-	console.log(newly_infectedData);
-	console.log(total_infectedData);
 	
-   Highcharts.chart('container', {
+	const res = await fetch("http://sos2021-25.herokuapp.com/api/v1/evictions?year=2018&location=andalusia");
+    desaData = await res.json();
+    if (res.ok) {
+	
+      desaData.forEach(stat => {
+	  
+	  totalData.push(parseInt(stat.total));
+      
+      });
+    }
+	
+	for(let o of evictionData){
+		cont=cont+o;
+	}
+    
+    console.log("evictionData"+evictionData);
+	console.log("contador"+cont);
+	console.log("total"+totalData);
+	
+   var chart = Highcharts.chart('container', {
+
     chart: {
-        type: 'column',
-        options3d: {
-            enabled: true,
-            alpha: 15,
-            beta: 15,
-            viewDistance: 25,
-            depth: 40
-        }
+        type: 'column'
     },
 
     title: {
-        text: 'GRUPO 24 Administrador de datos de Niños/as con VIH'
+        text: 'Highcharts responsive chart'
+    },
+
+    subtitle: {
+        text: 'Resize the frame or click buttons to change appearance'
+    },
+
+    legend: {
+        align: 'right',
+        verticalAlign: 'middle',
+        layout: 'vertical'
     },
 
     xAxis: {
-        categories: yearData,
+        categories: ano,
         labels: {
-            skew3d: true,
-            style: {
-                fontSize: '16px'
-            }
+            x: -10
         }
     },
 
     yAxis: {
         allowDecimals: false,
-        min: 0,
         title: {
-            text: 'Numero de personas',
-            skew3d: true
-        }
-    },
-
-    tooltip: {
-        headerFormat: '<b>{point.key}</b><br>',
-        pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: {point.y} / {point.stackTotal}'
-    },
-
-    plotOptions: {
-        column: {
-            stacking: 'normal',
-            depth: 40
+            text: 'Amount'
         }
     },
 
     series: [{
-        name: 'Viviendo con VIH',
-        data: living_withData,
-        //stack: 'male'
+        name: 'suma de deshaucios de algunas provincias de andalucia',
+        data: cont
     }, {
-        name: 'Nuevos infectados',
-        data: newly_infectedData,
-        //stack: 'male'
-    }, {
-        name: 'Total de infectados',
-        data: total_infectedData,
-        //stack: 'female'
-    }]
+        name: 'total de deshaucios de andalucia',
+        data: totalData
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    layout: 'horizontal'
+                },
+                yAxis: {
+                    labels: {
+                        align: 'left',
+                        x: 0,
+                        y: -5
+                    },
+                    title: {
+                        text: null
+                    }
+                },
+                subtitle: {
+                    text: null
+                },
+                credits: {
+                    enabled: false
+                }
+            }
+        }]
+    }
 });
+
 	
-  }
+ }
+ 
 </script>
 <svelte:head>
 
@@ -162,17 +189,7 @@ let buy_sell_Chart_eviction_Data = [];
     {/if}
   </div>
 
-  <div>
-  
-    <figure class="highcharts-figure">
-    <div id="container"></div>
-    <p class="highcharts-description">
-        Chart showing grouped and stacked 3D columns. These features are
-        available both for 2D and 3D column charts.
-    </p>
-</figure>
-	
-  </div>
+
 </main>
 
 <style>
@@ -201,38 +218,45 @@ let buy_sell_Chart_eviction_Data = [];
 }
 
 .highcharts-figure, .highcharts-data-table table {
-  min-width: 310px; 
-  max-width: 800px;
-  margin: 1em auto;
+    min-width: 310px; 
+    max-width: 800px;
+    margin: 1em auto;
 }
 
 .highcharts-data-table table {
-  font-family: Verdana, sans-serif;
-  border-collapse: collapse;
-  border: 1px solid #EBEBEB;
-  margin: 10px auto;
-  text-align: center;
-  width: 100%;
-  max-width: 500px;
+	font-family: Verdana, sans-serif;
+	border-collapse: collapse;
+	border: 1px solid #EBEBEB;
+	margin: 10px auto;
+	text-align: center;
+	width: 100%;
+	max-width: 500px;
 }
 .highcharts-data-table caption {
-  padding: 1em 0;
-  font-size: 1.2em;
-  color: #555;
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
 }
 .highcharts-data-table th {
-  font-weight: 600;
-  padding: 0.5em;
+	font-weight: 600;
+    padding: 0.5em;
 }
 .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-  padding: 0.5em;
+    padding: 0.5em;
 }
 .highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-  background: #f8f8f8;
+    background: #f8f8f8;
 }
 .highcharts-data-table tr:hover {
-  background: #f1f7ff;
+    background: #f1f7ff;
 }
+
+#button-bar {
+    min-width: 310px;
+    max-width: 800px;
+    margin: 0 auto;
+}
+
 
 
 </style>
