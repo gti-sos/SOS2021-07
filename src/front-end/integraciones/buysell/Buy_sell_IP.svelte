e1282602429932559d804bd28eec4e0708c72e0f
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 03:24:51 +0000

    reubicando cosas18

--- src/front-end/integraciones/buysell/Buy_sell_IP.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_IP.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { Table, Button, Nav, NavItem, NavLink } from "sveltestrap";
-  import Chart from "chart.js";
+  import chart from "chart.js";
   
   let Data2 = [];
   let evictionData = [];
