8d6ccad6a38ac0297a0c94faaa1b2f938d90f9d6
RoundPegInASquareHole <alex_pariente@hotmail.es>
2021-06-07 21:13:35 +0200

    video ale

--- src/front-end/complementos/Acerca_de.svelte
+++ src/front-end/complementos/Acerca_de.svelte
@@ -42,7 +42,7 @@
     <iframe
       width="400"
       height="200"
-      src="https://www.youtube.com/embed/TncasA0usUA"
+      src="https://www.youtube.com/embed/ZprLapvV3zk"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
