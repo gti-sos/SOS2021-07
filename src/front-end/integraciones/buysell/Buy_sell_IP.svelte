79ab12f414b616a00847a29efb515e3db63c90df
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 02:21:49 +0000

    reubicando cosas7

--- src/front-end/integraciones/buysell/Buy_sell_IP.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_IP.svelte
@@ -1,6 +1,10 @@
 <script>
   import { onMount } from "svelte";
   import { Table, Button, Nav, NavItem, NavLink } from "sveltestrap";
+  
+  let Data2 = [];
+  let evictionData = [];
+let provinceData=[];
 
   let errorMsg = "";
   let okMsg = "";
@@ -11,65 +15,105 @@
     });
   }
   
-  async function loadChart() {
+  //loadinitial de mis datos
   
+  async function loadStats2() {
+    
+    const res = await fetch(
+     "https://sos2021-07.herokuapp.com/api/v2/buy_sell/loadInitialData"
+    ).then(function (res) {
+      if (res.ok) {
+        getStats();
+        errorMsg = "";
+        okMsg = "Datos cargados correctamente";
+        console.log("OK");
+      } else {
+        if (res.status === 500) {
+          errorMsg = "No se ha podido acceder a la base de datos";
+        }
+        okMsg = "";
+        console.log("ERROR!" + errorMsg);
+      }
+    });
+  }
+  
+  loadStats2();
+  
+  
+  async function loadChart() {
+  		
+		//API EXT IP
+		
   		const extfetch = await fetch("http://api.ipify.org/?format=json");
         const ipaddr = await extfetch.json();
         var ipfin = ipaddr.ip;
-        console.log(ipfin)
+        console.log(ipfin);
+		
+		const res2 = await fetch("https://sos2021-07.herokuapp.com/api/v2/buy_sell?offset=1&limit=6");
+		let Data2 = await res2.json();
+		
+		Data2.forEach(stat => {
+	  
+	  evictionData.push(parseInt(stat.eviction));
+	  provinceData.push(stat.province);
+
+      });
+	  
+	  console.log(evictionData);
+	console.log(provinceData);
   
-  Highcharts.chart('container', {
+ Highcharts.chart('container', {
+
+    chart: {
+        polar: true,
+        type: 'line'
+    },
+
+    accessibility: {
+        description: 'A spiderweb chart compares the allocated budget against actual spending within an organization. The spider chart has six spokes. Each spoke represents one of the 6 departments within the organization: sales, marketing, development, customer support, information technology and administration. The chart is interactive, and each data point is displayed upon hovering. The chart clearly shows that 4 of the 6 departments have overspent their budget with Marketing responsible for the greatest overspend of $20,000. The allocated budget and actual spending data points for each department are as follows: Sales. Budget equals $43,000; spending equals $50,000. Marketing. Budget equals $19,000; spending equals $39,000. Development. Budget equals $60,000; spending equals $42,000. Customer support. Budget equals $35,000; spending equals $31,000. Information technology. Budget equals $17,000; spending equals $26,000. Administration. Budget equals $10,000; spending equals $14,000.'
+    },
 
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'Budget vs spending',
+        x: -80
     },
 
-    subtitle: {
-        text: 'Source: thesolarfoundation.com'
-    },
-
-    yAxis: {
-        title: {
-            text: 'Number of Employees'
-        }
+    pane: {
+        size: '80%'
     },
 
     xAxis: {
-        accessibility: {
-            rangeDescription: 'Range: 2010 to 2017'
-        }
+        categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
+            'Information Technology', 'Administration'],
+        tickmarkPlacement: 'on',
+        lineWidth: 0
+    },
+
+    yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0
+    },
+
+    tooltip: {
+        shared: true,
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
     },
 
     legend: {
-        layout: 'vertical',
         align: 'right',
-        verticalAlign: 'middle'
-    },
-
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2010
-        }
+        verticalAlign: 'middle',
+        layout: 'vertical'
     },
 
     series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+        name: 'Allocated Budget',
+        data: [43000, 19000, 60000, 35000, 17000, 10000],
+        pointPlacement: 'on'
     }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+        name: 'Actual Spending',
+        data: [50000, 39000, 42000, 31000, 26000, 14000],
+        pointPlacement: 'on'
     }],
 
     responsive: {
@@ -79,9 +123,12 @@
             },
             chartOptions: {
                 legend: {
-                    layout: 'horizontal',
                     align: 'center',
-                    verticalAlign: 'bottom'
+                    verticalAlign: 'bottom',
+                    layout: 'horizontal'
+                },
+                pane: {
+                    size: '70%'
                 }
             }
         }]
@@ -146,9 +193,9 @@
     <figure class="highcharts-figure">
     <div id="container"></div>
     <p class="highcharts-description">
-        Basic line chart showing trends in a dataset. This chart includes the
-        <code>series-label</code> module, which adds a label to each line for
-        enhanced readability.
+        A spiderweb chart or radar chart is a variant of the polar chart.
+        Spiderweb charts are commonly used to compare multivariate data sets,
+        like this demo using six variables of comparison.
     </p>
 </figure>
 	
@@ -181,8 +228,8 @@
 }
 
 .highcharts-figure, .highcharts-data-table table {
-    min-width: 360px; 
-    max-width: 800px;
+    min-width: 320px;
+    max-width: 700px;
     margin: 1em auto;
 }
 
