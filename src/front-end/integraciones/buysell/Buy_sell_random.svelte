79396ae3517af9b01abf0f9295c80e280004cacb
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 09:29:11 +0000

    segundo intento3

--- src/front-end/integraciones/buysell/Buy_sell_random.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_random.svelte
@@ -10,21 +10,6 @@ let buy_sell_Data = [];
 let buy_sell_Chart_comunityprovinceyear_Data = [];
 let estrin='';
 
-    onMount(loadQuote);
-	//esta funcion me carga la api externa
-    async function loadQuote(){
-        const resData = await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=es", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "quotes15.p.rapidapi.com",
-                "x-rapidapi-key": "15f4056b92mshce2e627ff99101dp16fe2ajsn4bc9dff05513"
-            }
-        })
-        const quoteData = await resData.json(); 
-        cita = quoteData["content"];
-        autor = quoteData["originator"]["name"];
-		
-    }
 	//FUNCION QUE ME CARGA LAS COSAS
 	async function loadStats2() {
     
@@ -49,7 +34,7 @@ let estrin='';
   loadStats2();
   
   async function loadChart() {
-  
+  //PARA LA MIA
   const res = await fetch(BASE_CONTACT_API_PATH + "/buy_sell");
     buy_sell_Data = await res.json();
     if (res.ok) {
@@ -58,6 +43,17 @@ let estrin='';
 	  estrin=estrin+stat.autonomous_community+" "+stat.province+" ";
       //buy_sell_Chart_comunityprovinceyear_Data.push(stat.autonomous_community);
       });
+	  //PARA LA EXT
+	  const resData = await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=es", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "quotes15.p.rapidapi.com",
+                "x-rapidapi-key": "15f4056b92mshce2e627ff99101dp16fe2ajsn4bc9dff05513"
+            }
+        })
+        const quoteData = await resData.json(); 
+        cita = quoteData["content"];
+        autor = quoteData["originator"]["name"];
 	  
 	  estrin=estrin+cita;
 	  estrin=estrin+autor;
