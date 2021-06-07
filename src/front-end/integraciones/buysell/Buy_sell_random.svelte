900959521304628dedf7001060a9cfbc760be629
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 09:43:33 +0000

    segundo intento6

--- src/front-end/integraciones/buysell/Buy_sell_random.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_random.svelte
@@ -105,11 +105,11 @@ Highcharts.chart('container', {
 
 <svelte:head>
 
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/wordcloud.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"on:load={loadStats2}></script>
+<script src="https://code.highcharts.com/highcharts.js"on:load={loadStats2}></script>
+<script src="https://code.highcharts.com/modules/wordcloud.js"on:load={loadStats2}></script>
+//<script src="https://code.highcharts.com/modules/exporting.js"></script>
+//<script src="https://code.highcharts.com/modules/export-data.js"></script>
+//<script src="https://code.highcharts.com/modules/accessibility.js"on:load={loadStats2}></script>
 	
 </svelte:head>
 
