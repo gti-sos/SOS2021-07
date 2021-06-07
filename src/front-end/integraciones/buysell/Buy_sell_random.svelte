686af0bab644a7b840389106d85270e0fdd8d92c
nuresctor <escamillatorresnuria@gmail.com>
2021-06-07 10:25:53 +0000

    ultimo intento4

--- src/front-end/integraciones/buysell/Buy_sell_random.svelte
+++ src/front-end/integraciones/buysell/Buy_sell_random.svelte
@@ -10,41 +10,10 @@ let buy_sell_Data = [];
 let buy_sell_Chart_comunityprovinceyear_Data = [];
 let estrin='';
 
-	//FUNCION QUE ME CARGA LAS COSAS
-	async function loadStats2() {
-    
-    const res = await fetch(
-     "https://sos2021-07.herokuapp.com/api/v2/buy_sell/loadInitialData"
-    ).then(function (res) {
-      if (res.ok) {
-        loadChart();
-        errorMsg = "";
-        okMsg = "Datos cargados correctamente";
-        console.log("OK");
-      } else {
-	  loadChart();
-        if (res.status === 500) {
-          errorMsg = "No se ha podido acceder a la base de datos";
-        }
-      }
-    });
-  }
-  
-  //llamada de loadstats2
- 
-  
-  async function loadChart() {
-  //PARA LA MIA
-  const res = await fetch(BASE_CONTACT_API_PATH + "/buy_sell");
-    buy_sell_Data = await res.json();
-    if (res.ok) {
 	
-      buy_sell_Data.forEach(stat => {
-	  estrin=estrin+stat.autonomous_community+" "+stat.province+" ";
-      //buy_sell_Chart_comunityprovinceyear_Data.push(stat.autonomous_community);
-      });
-	  //PARA LA EXT
-	  const resData = await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=es", {
+  onMount(loadQuote);
+    async function loadQuote(){
+        const resData = await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=es", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "quotes15.p.rapidapi.com",
@@ -54,163 +23,20 @@ let estrin='';
         const quoteData = await resData.json(); 
         cita = quoteData["content"];
         autor = quoteData["originator"]["name"];
-	  
-	  estrin=estrin+cita;
-	  estrin=estrin+autor;
-	  console.log(estrin);
-	  
-	  var text = estrin;
-var lines = text.split(/[,\. ]+/g),
-    data = Highcharts.reduce(lines, function (arr, word) {
-        var obj = Highcharts.find(arr, function (obj) {
-            return obj.name === word;
-        });
-        if (obj) {
-            obj.weight += 1;
-        } else {
-            obj = {
-                name: word,
-                weight: 1
-            };
-            arr.push(obj);
-        }
-        return arr;
-    }, []);
+    }
+  
 
-Highcharts.chart('container', {
-    accessibility: {
-        screenReaderSection: {
-            beforeChartFormat: '<h5>{chartTitle}</h5>' +
-                '<div>{chartSubtitle}</div>' +
-                '<div>{chartLongdesc}</div>' +
-                '<div>{viewTableButton}</div>'
-        }
-    },
-    series: [{
-        type: 'wordcloud',
-        data: data,
-        name: 'Occurrences'
-    }],
-    title: {
-        text: 'Integracion BUY-EXT RANDOM'
-    }
-});
-	  
-    }
-  	
-  }
 	
 	
 </script>
 
-<svelte:head>
-
-<script src="https://code.highcharts.com/highcharts.js"on:load={loadStats2}></script>
-<script src="https://code.highcharts.com/modules/wordcloud.js"on:load={loadStats2}></script>
-//<script src="https://code.highcharts.com/modules/exporting.js"></script>
-//<script src="https://code.highcharts.com/modules/export-data.js"></script>
-//<script src="https://code.highcharts.com/modules/accessibility.js"on:load={loadStats2}></script>
-	
-</svelte:head>
-
 <main>
 
-  <Nav>
-        <NavItem>
-          <NavLink href="/"><Button color="primary">Pรกgina Inicial</Button></NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/#/integrations"><Button color="primary">Volver</Button></NavLink>
-        </NavItem>
-    </Nav>
-
-  <div>
-    <h2>
-      Analíticas
-    </h2>
-  </div>
-
-  <div>
-    {#if errorMsg}
-      <p class="msgRed" style="color: #9d1c24">ERROR: {errorMsg}</p>
-    {/if}
-    {#if okMsg}
-      <p class="msgGreen" style="color: #155724">{okMsg}</p>
-    {/if}
-  </div>
-
-  <div>
+<br>
+<blockquote class="blockquote text-center">
+    
+    <h1 class="display-4">{cita}</h1>
+    <footer class="blockquote-footer"> <cite title="Source Title">{autor}</cite></footer>
+  </blockquote>
   
-    <figure class="highcharts-figure">
-    <div id="container"></div>
-    <p class="highcharts-description">
-        Word clouds are used to visualize how often each word in a
-        text occurs. This example uses an excerpt from the popular
-        "Lorem Ipsum" text. Words that appear often will appear
-        larger.
-    </p>
-</figure>
-	
-  </div>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-  div{
-    margin-bottom: 15px;
-  }
-  p {
-    display: inline;
-  }
-  .msgRed {
-    padding: 8px;
-    background-color: #f8d7da;
-  }
-  .msgGreen {
-    padding: 8px;
-    background-color: #d4edda;
-  }
-  
-  #container {
-  height: 400px; 
-}
-
-.highcharts-figure, .highcharts-data-table table {
-    min-width: 320px; 
-    max-width: 800px;
-    margin: 1em auto;
-}
-
-.highcharts-data-table table {
-	font-family: Verdana, sans-serif;
-	border-collapse: collapse;
-	border: 1px solid #EBEBEB;
-	margin: 10px auto;
-	text-align: center;
-	width: 100%;
-	max-width: 500px;
-}
-.highcharts-data-table caption {
-    padding: 1em 0;
-    font-size: 1.2em;
-    color: #555;
-}
-.highcharts-data-table th {
-	font-weight: 600;
-    padding: 0.5em;
-}
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-    padding: 0.5em;
-}
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-    background: #f8f8f8;
-}
-.highcharts-data-table tr:hover {
-    background: #f1f7ff;
-}
-
-</style>
+ </main>
